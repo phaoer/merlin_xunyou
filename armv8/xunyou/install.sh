f34ed0475d48b7594e12ba435c31f1169823b038
liujin <liujin@xunyou.com>
2020-07-03 18:54:16 +0800

    update

--- armv8/xunyou/install.sh
+++ armv8/xunyou/install.sh
@@ -3,6 +3,7 @@ MODULE=xunyou
 title="迅游加速器"
 VERSION="1.0.0.3"
 systemType=0
+clientType="0"
 
 remove_install_file(){
     rm -rf /tmp/${MODULE}*.gz > /dev/null 2>&1
@@ -40,6 +41,7 @@ case $(uname -m) in
         ;;
 esac
 
+[[ "${1}" == "app" || "${1}" == "APP" ]] && clientType="1"
 
 if [ -d "/koolshare" ];then
     systemType=0
@@ -51,13 +53,12 @@ fi
 koolshare_install()
 {
     enable=`dbus get ${MODULE}_enable`
-    [ "${enable}" == "1" ] && sh /koolshare/xunyou/scripts/${MODULE}_config.sh stop
     #
-    [ -d "/koolshare/xunyou" ] && rm -rf /koolshare/xunyou
+    [ -e "/koolshare/scripts/uninstall_xunyou.sh" ] && sh /koolshare/scripts/uninstall_xunyou.sh
     mkdir -p /koolshare/xunyou
     #
-    rm -rf /koolshare/init.d/S90XunYouAcc.sh > /dev/null 2>&1
-    rm -rf /koolshare/scripts/xunyou_status.sh > /dev/null 2>&1
+    [ -z "${enable}" ] && dbus set ${MODULE}_enable="0"
+    [[ "${enable}" == "1" || "${clientType}" == "1" ]] && dbus set ${MODULE}_enable="1" && enable="1"
     #
     cp -rf /tmp/${MODULE}/webs/* /koolshare/webs/
     cp -rf /tmp/${MODULE}/res/*  /koolshare/res/
@@ -72,7 +73,6 @@ koolshare_install()
     #
     dbus set ${MODULE}_version="${VERSION}"
     dbus set ${MODULE}_title="${title}"
-    [ "${enable}" != "1" ] && dbus set ${MODULE}_enable="0"
     dbus set softcenter_module_${MODULE}_install=1
     dbus set softcenter_module_${MODULE}_name=${MODULE}
     dbus set softcenter_module_${MODULE}_version="${VERSION}"
